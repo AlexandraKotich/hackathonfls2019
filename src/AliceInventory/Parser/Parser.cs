dc0c6a9ed68c532fc5a62a8d463d91001d9a449e
Sandra <Sandra@DESKTOP-R7RRCF7>
2019-05-21 10:35:31 +0300

    test logic

--- src/AliceInventory/Parser/Parser.cs
+++ src/AliceInventory/Parser/Parser.cs
@@ -7,11 +7,80 @@ namespace ConsoleApp
 {
     class Parser
     {
+        public static char[] Separators = { ' ', ',', '.', '\n', '\"' };
+        enum endofverbs { ишь, ть, вь, или, ели, али, уй, уйте, ьте, ни };
+        enum addroots { лож, лаг, надбав, добав, ключ, велич, полн, прибав, присоед, плюс };
+        enum deleteroots { удал, убер, убра, меньш, вын };
+        enum deleteallroots { очист };
+        enum readallroots { покаж, вывест, вывед, показ, предст };
         public ChatResponse TryParse(string userText)
         {
+            ChatResponse chatResponse = new ChatResponse();
+            string actionItem = "";
+
+            try { var words = userText.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+
+                string actionItem = ActionItem(words);
+                chatResponse.userAction = actionItem;
+            }
+            catch (Exception exc)
+            {
+                chatResponse.userAction = Add;
+            }
+            int countItem = CountItem(words);
             //some analysis of userText
             //...
-            return new ChatResponse(ChatResponse.UserAction.Add, "someItem", 10, new Unit("kg"));
+            return chatResponse(chatResponse.userAction, "someItem", countItem, new Unit("kg"));
+        }
+
+        public string ActionItem (string[] words)
+        {
+            foreach (string stringwrite in words)
+            {
+                if (stringwrite.Contains(endofverbs))
+                {
+                    if (stringwrite.Contains(addroots))
+                    {
+                        chatResponse.userAction = Add;
+                    }
+                    else if (stringwrite.Contains(deleteroots))
+                    {
+                        chatResponse.userAction = deleteLast;
+                    }
+                    else if (stringwrite.Contains(deleteallroots))
+                    {
+                        chatResponse.userAction = clearAll;
+                    }
+                    else if (stringwrite.Contains(readallroots))
+                    {
+                        chatResponse.userAction = readAll;
+                    }
+                }
+                else
+                {
+                    chatResponse.userAction = Add;
+                }
+            }
+        }
+
+        public double CountItem (string[] words)
+        {
+            double count = 1;
+            foreach (string countstring in words)
+            try {
+                    Int32.TryParse(countstring, out count);
+            }
+                catch (Exception exc)
+                {
+
+                }
+            return count; 
+        }
+        public Unit UnitItem (string[] words)
+        {
+            Unit unit = new Unit();
+
+            return unit;
         }
     }
 }
--- src/AliceInventory/Parser/Unit.cs
+++ src/AliceInventory/Parser/Unit.cs
@@ -2,21 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace ConsoleApp
 {
     public class Unit
     {
+
+        //this class for number units of objects 
         public string UnitName { get; set; }
+        
 
         public Unit(string unitName)
         {
-            UnitName = unitName;
+            Dictionary<string, string> unitDictionary = GetSynonyms();
+            try { UnitName = unitDictionary[unitName]; }
+            catch (Exception exc)
+            {
+                UnitName = "шт";
+            }
+
         }
         //not sure, perhaps this method implements some part of parser analysis funcion TryParse()
-        public List<string> GetSynonyms()
+        public Dictionary <string,string> GetSynonyms() 
         {
-            return new List<string>();
+            Dictionary<string, string> unitDictionary = new Dictionary<string, string>();
+            unitDictionary.Add("килограмм", "кг");
+            unitDictionary.Add("килограмма", "кг");
+            unitDictionary.Add("килограммов", "кг");
+            unitDictionary.Add("кг", "кг");
+            unitDictionary.Add("г", "г");
+            unitDictionary.Add("грамм", "г");
+            unitDictionary.Add("граммов", "г");
+            unitDictionary.Add("миллиграммов", "мг");
+            unitDictionary.Add("миллиграмм", "мг");
+            unitDictionary.Add("миллиграмма", "мг");
+            unitDictionary.Add("литров", "л");
+            unitDictionary.Add("литр", "л");
+            unitDictionary.Add("литра", "л");
+            unitDictionary.Add("миллилитры", "мл");
+            unitDictionary.Add("миллилитров", "мл");
+            unitDictionary.Add("миллилитра", "мл");
+            unitDictionary.Add("метра", "м");
+            unitDictionary.Add("метров", "м");
+            unitDictionary.Add("метр", "м");
+            unitDictionary.Add("миллиметр", "мм");
+            unitDictionary.Add("миллиметров", "мм");
+            unitDictionary.Add("миллиметра", "мм");
+            unitDictionary.Add("сантиметра", "см");
+            unitDictionary.Add("сантиметров", "см");
+            unitDictionary.Add("сантиметр", "см");
+            unitDictionary.Add("штук", "шт");
+            unitDictionary.Add("штуки", "шт");
+            unitDictionary.Add("штука", "шт");
+            return  unitDictionary;
         }
 
         public override string ToString()
